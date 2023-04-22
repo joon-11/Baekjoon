040be27cbc7a387121b839a1f17a760899ca0703
joon-11 <123048555+joon-11@users.noreply.github.com>
2023-04-22 21:47:35 +0900

    [Bronze V] Title: 합, Time: 204 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int k = 0;
+		for(int i = 1; i < a+1; i++) {
+			k += i;
+		}
+		System.out.println(k);
+	}
+
+}
