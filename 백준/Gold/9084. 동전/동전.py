292106d5ebe2da4a9e59730cc8c040dab86cc7b3
joon-11 <123048555+joon-11@users.noreply.github.com>
2023-05-08 13:47:02 +0900

    [Gold V] Title: 동전, Time: 80 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.py
@@ -0,0 +1,13 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    coins = list(map(int, input().split()))
+    m = int(input())
+    d = [0] * (m + 1)
+    d[0] = 1
+    for coin in coins:
+        for i in range(m + 1):
+            if i >= coin:
+                d[i] += d[i - coin]
+
+    print(d[m])
